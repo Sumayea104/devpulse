438699595367d662edd1201c691ad6e7568b6825
Sumayea Rahman <shorbonam104@gmail.com>
2026-05-24 12:36:15 +0600

    Add dotenv import to database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,14 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // এই লাইনটি যোগ করুন
+  },
 });
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
-
-
 export default pool;
